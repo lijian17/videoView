645d9be34be948e92a59fa79c0e35854dd16bd4a
lijian17 <374452668@qq.com>
2017-06-25 16:37:20 +0800

    第一版本： 使用android自带VideoView和MediaController实现视频的播放和控制 1、本地视频的播放 2、远程视频的播放

--- videoplayer/src/net/dxs/videoplayer/MainActivity.java
+++ videoplayer/src/net/dxs/videoplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package net.dxs.videoplayer;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends Activity {
@@ -25,7 +28,40 @@ public class MainActivity extends Activity {
 	}
 
 	private void initData() {
+		// playLocalVideo();
+		playInternetVideo();
 
+		initMediaController();
 	}
 
+	/**
+	 * 初始化视频播放控制器
+	 */
+	private void initMediaController() {
+		// 使用MediaController控制视频播放
+		MediaController controller = new MediaController(this);
+
+		// 设置VideoView与MediaController建立关联
+		mVv_videoPlay.setMediaController(controller);
+
+		// 设置MediaController与VideoView建立关联
+		controller.setMediaPlayer(mVv_videoPlay);
+	}
+
+	/**
+	 * 播放本地视频
+	 */
+	private void playLocalVideo() {
+		String path = Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/father.mp4";
+		mVv_videoPlay.setVideoPath(path);
+	}
+
+	/**
+	 * 播放网络视频
+	 */
+	private void playInternetVideo() {
+		String uriString = "http://video-js.zencoder.com/oceans-clip.mp4";
+		mVv_videoPlay.setVideoURI(Uri.parse(uriString));
+	}
 }
